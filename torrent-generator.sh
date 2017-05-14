e9b4ca4bffe81c942bd7b27bb5cce69eabc3a19b
Lorenzo Faletra <palinuro@parrotsec.org>
2017-05-14 14:24:36 +0000

    Update torrent-generator.sh

--- torrent-generator.sh
+++ torrent-generator.sh
@@ -15,27 +15,40 @@ echo "creating torrent for $ISOFILE"
 -a udp://tracker.ilibr.org:80/announce \
 -a udp://tracker.coppersurfer.tk:6969 \
 -c "Parrot Security OS official torrent - don't seed it if an updated version is available. for security reasons we don't want old releases to be still used" \
--w http://www.parrotsec.org/get/mit/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/jmu/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/clarkson/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/berkeley/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/garr/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/rwth-aachen/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/warwick/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/nluug/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/onet/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/umu/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/heanet/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/esslingen/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/uoc/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/babylon-fr/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/babylon-nl/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/yandex/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/amberit/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/nchc/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/ustc/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/0x/$PATH/$ISOFILE,\
-http://www.parrotsec.org/get/tuna/$PATH/$ISOFILE,\
+-w http://mirrors.ocf.berkeley.edu/parrot/$PATH/$ISOFILE,\
+http://mirrors.mit.edu/parrot/$PATH/$ISOFILE,\
+http://mirror.jmu.edu/pub/parrot/$PATH/$ISOFILE,\
+http://mirror.clarkson.edu/parrot/$PATH/$ISOFILE,\
+http://mirror.cedia.org.ec/parrot/$PATH/$ISOFILE,\
+http://mirror.uta.edu.ec/parrot/$PATH/$ISOFILE,\
+http://mirror.ueb.edu.ec/parrot/$PATH/$ISOFILE,\
+http://ba.mirror.garr.it/mirrors/parrot/$PATH/$ISOFILE,\
+http://ct.mirror.garr.it/mirrors/parrot/$PATH/$ISOFILE,\
+http://ftp.halifax.rwth-aachen-de/parrotsec/$PATH/$ISOFILE,\
+http://anorien.csc.warwick.ac.uk/mirrors/parrot/$PATH/$ISOFILE,\
+http://ftp.nluug.nl/os/Linux/distr/parrot/$PATH/$ISOFILE,\
+http://mirror.onet.pl/pub/mirrors/parrot/$PATH/$ISOFILE,\
+http://ftp.acc.umu.se/mirror/parrotsec.org/parrot/$PATH/$ISOFILE,\
+http://ftp.heanet.ie/pub/parrotsec/$PATH/$ISOFILE,\
+https://ftp-stud.hs-esslingen.de/pub/Mirrors/archive.parrotsec.org/$PATH/$ISOFILE,\
+http://dasa1-euro.archive.parrotsec.org/parrot$PATH/$ISOFILE,\
+http://ftp.cc.uoc.gr/mirrors/linux/parrot/$PATH/$ISOFILE,\
+https://fr.mirror.babylon.network/parrot/$PATH/$ISOFILE,\
+https://nl.mirror.babylon.network/parrot/$PATH/$ISOFILE,\
+http://ftp.belnet.be/archive.parrotsec.org/$PATH/$ISOFILE,\
+http://mirrors.up.pt/parrot/$PATH/$ISOFILE,\
+http://mirrors.dotsrc.org/parrot-iso/$PATH/$ISOFILE,\
+http://mirror.yandex.ru/mirrors/parrot/$PATH/$ISOFILE,\
+http://mirror.dhakacom.com.bd/parrotsec/$PATH/$ISOFILE,\
+http://free.nchc.org.tw/parrot/$PATH/$ISOFILE,\
+http://mirror.0x.sg/parrot/$PATH/$ISOFILE,\
+http://mirrors.ustc.edu.cn/parrot/$PATH/$ISOFILE,\
+https://mirrors.tuna.tsinghua.edu.cn/parrot/$PATH/$ISOFILE,\
+http://parrot.asis.io/$PATH/$ISOFILE,\
+http://mirror.lagoon.nc/pub/parrot/$PATH/$ISOFILE,\
+http://mirror.kku.ac.th/parrot/$PATH/$ISOFILE,\
+http://parrot-na.archive.parrotsec.org/parrot/$PATH/$ISOFILE,\
+http://mirrordirector.archive.parrotsec.org/parrot/$PATH/$ISOFILE,\
 \
 http://citylan.dl.sourceforge.net/project/parrotsecurity/$PATH/$ISOFILE,\
 http://colocrossing.dl.sourceforge.net/project/parrotsecurity/$PATH/$ISOFILE,\
